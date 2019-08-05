d7ca92d9043c4756e6fd4836a8da2577a5678e78
erba <eric.bachard@free.fr>
2019-08-05 23:10:58 +0200

    Add some files. License issues remain to be fixed

---
+++ Sources/src/audiodevices_helper.cpp
@@ -0,0 +1,85 @@
+#include "application.hpp"
+#include "audiodevices.hpp"
+#include <vector>
+#include <cstring>
+#include "imgui.h"
+#include "ffmpeg_player.hpp"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <objbase.h>
+#endif
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_thread.h>
+
+
+static short int count = 0;
+static std::string currentRecordableAudioDevice = "";  // default device
+
+extern SDL_AudioDeviceID audio_dev;
+extern std::vector<std::string> recordableAudioDeviceNames;
+
+namespace md
+
+{
+    short int getSDLRecordableAudioDeviceNames(void)
+    {
+        while (SDL_GetAudioDeviceName(count, 1) != nullptr)
+        {
+            recordableAudioDeviceNames.push_back(SDL_GetAudioDeviceName(count, 1));
+            std::cout << "Audio device (with recording capability) device number : " << count  << " name : "  <<  SDL_GetAudioDeviceName(count, 1) << std::endl;
+            count++;
+
+            if (count > 4)
+                break;
+        }
+        return count;
+    }
+
+    bool selectSDLRecordableAudioDeviceName(short int aDevice)
+    {
+        if (aDevice >= count)
+        {
+            std::cerr << "Wrong device" << std::endl;
+            return false;
+        }
+        currentRecordableAudioDevice = recordableAudioDeviceNames.at(aDevice);
+        return true;
+    }
+
+    std::string getCurrentRecordableAudioDevice(void)
+    {
+        return currentRecordableAudioDevice;
+    }
+
+    int audio_open(void *arg, int64_t wanted_channel_layout, int wanted_nb_channels, int wanted_sample_rate)
+    {
+        // define audio specs containers
+        SDL_AudioSpec wanted_specs;
+        SDL_AudioSpec specs;
+
+        // set audio settings
+        wanted_specs.freq = wanted_sample_rate;
+        wanted_specs.format = AUDIO_S16SYS;
+        wanted_specs.channels = wanted_nb_channels;
+        wanted_specs.silence = 0;
+        wanted_specs.samples = SDL_AUDIO_MIN_BUFFER_SIZE * 2;
+        wanted_specs.callback = audio_callback;
+        wanted_specs.userdata = arg;
+
+        // https://trac.ffmpeg.org/ticket/6891
+        #ifdef _WIN32
+        CoInitialize(NULL);
+        #endif
+
+        if (!(audio_dev = SDL_OpenAudioDevice((const char *)currentRecordableAudioDevice.c_str(), 0, &wanted_specs, &specs, SDL_AUDIO_ALLOW_FREQUENCY_CHANGE | SDL_AUDIO_ALLOW_CHANNELS_CHANGE)))
+        {
+            std::cout << "[audio]could not open audio, error:" << SDL_GetError() << std::endl;
+            return -1;
+        }
+        return  specs.size;
+    }
+
+
+} /* end namespace md */
---
+++ Sources/src/capturedev.cpp
@@ -0,0 +1,232 @@
+#include <SDL2/SDL.h>
+#include "capturedev.hpp"
+#include "application.hpp"
+
+#ifndef NATIVE_BUILD
+    #ifndef HAVE_VIDEOINPUT
+    #define HAVE_VIDEOINPUT
+    #endif
+    #ifndef HAVE_DSHOW
+    #define HAVE_DSHOW
+    #endif
+#endif
+
+CaptureDev::CaptureDev()
+{
+    mb_opened = false;
+    md_delay = 1;
+    //maDevNumber = DEFAULT_DEVICE_NUMBER;
+}
+
+CaptureDev::~CaptureDev()
+{
+    Close();
+}
+
+//bool CaptureDev::Init(int aDevNumber)
+bool CaptureDev::Init(int aDevice)
+{
+    if (maVideoCap.isOpened())
+        maVideoCap.release();
+
+    mb_opened = false;
+
+    maDevNumber = aDevice;
+
+#ifdef NATIVE_BUILD
+    // Linux
+    // FIXME : propose a list of active devices
+    // + FIXME : add other possibilities, e.g. using GSTREAMER (cv::CAP_GSTREAMER)
+//    maVideoCap.open(maDevNumber + cv::CAP_GSTREAMER);
+    maVideoCap.open(maDevNumber + cv::CAP_V4L2);
+#else
+    // Windows
+    // http://docs.opencv.org/trunk/d0/da7/videoio_overview.html
+    maVideoCap.open(maDevNumber + cv::CAP_DSHOW);
+#endif /* NATIVE_BUILD */
+
+    if (!maVideoCap.isOpened() || !bFirstFrameSuccess())
+        std::cerr << "***Could not initialize capturing...***" << std::endl;
+
+#if defined(_WIN32) || defined (WIN32)
+    // works only on Windows
+    setFourCC(CV_FOURCC('M','J','P','G'));
+#endif
+
+    initializeFrameSize();
+
+#ifdef NATIVE_BUILD
+    setCaptureFPS(LINUX_DEFAULT_FRAMERATE);
+#endif
+
+    mfCaptureFPS = getCaptureFPS();
+
+    // FIXME : this has nothing to do there ...
+    // better put that in the VideoRecorder
+    //    mdStackSize = delay*mfCaptureFPS;
+
+    // keep a trace of the previous delay
+    //    short int delay_ref = delay;
+
+    // in case of failure during initialization, we return false
+    mb_opened = maVideoCap.isOpened();
+
+    return mb_opened;
+}
+
+void CaptureDev::Close()
+{
+    maVideoCap.release();
+}
+
+bool CaptureDev::bFirstFrameSuccess()
+{
+    bool toReturn = true;
+    cv::Mat firstFrame;
+    maVideoCap.retrieve(firstFrame);
+
+    if (!firstFrame.empty())
+    {
+        mb_opened = maVideoCap.read(firstFrame); // read a new frame from video
+        toReturn = mb_opened;
+    }
+    else
+        toReturn = false;
+
+    // avoid leaking
+    firstFrame.release();
+    return toReturn;
+}
+
+
+void CaptureDev::getFrame()
+{
+
+   bool bSuccess3 = maVideoCap.read(this->USBFrame);
+
+   if (!bSuccess3)
+       std::cerr << "Pb with cap2.getFrame()" << std::endl;
+}
+
+double CaptureDev::getCaptureFPS(void)
+{
+    return (maVideoCap.get(cv::CAP_PROP_FPS));
+}
+
+void CaptureDev::setDevNumber(int aDev)
+{
+  maDevNumber = aDev;
+}
+
+int CaptureDev::getDevNumber(void)
+{
+  return maDevNumber;
+}
+
+void CaptureDev::set_delay(int delay)
+{
+  md_delay = delay;
+}
+
+int CaptureDev::get_delay(void)
+{
+  return md_delay;
+}
+
+
+void CaptureDev::setFourCC(unsigned int FourCC)
+{
+    //maVideoCap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M','J','P','G'));
+    maVideoCap.set(CV_CAP_PROP_FOURCC, FourCC);
+}
+
+void CaptureDev::setCaptureFPS(int aValue)
+{
+    // depends on the webcam, but breaks everything on Windows
+    // Linux : 24 fps is a good value for i3 or i5 + 6GB of RAM 
+    //         30 fps for i7 + 8GB RAM or more
+    // tested OK with C920, C922, lifecam 3000
+    maVideoCap.set(cv::CAP_PROP_FPS, aValue);
+}
+
+void CaptureDev::initializeFrameSize(void)
+{
+    maVideoCap.set(cv::CAP_PROP_FRAME_WIDTH, CAPTURED_FRAME_WIDTH);
+    maVideoCap.set(cv::CAP_PROP_FRAME_HEIGHT, CAPTURED_FRAME_HEIGHT);
+
+    //namedWindow("MyVideo", CV_WINDOW_OPENGL);  (thread issue)
+    mfWidth  = maVideoCap.get(cv::CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+    mfHeight = maVideoCap.get(cv::CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+
+    //set framesize for use with videoWriter
+    if (mfWidth * mfHeight < 1.0 )
+    {
+        mfWidth  = CAPTURED_FRAME_WIDTH;
+        mfHeight = CAPTURED_FRAME_HEIGHT;
+    }
+
+    maFrameSize = cv::Size(static_cast<int>(mfWidth), static_cast<int>(mfHeight));
+}
+
+void CaptureDev::setFrameSize(double aWidth, double aHeight)
+{
+    maVideoCap.set(cv::CAP_PROP_FRAME_WIDTH, aWidth);
+    maVideoCap.set(cv::CAP_PROP_FRAME_HEIGHT, aHeight);
+
+    // check whether it works
+    mfWidth = maVideoCap.get(cv::CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+    mfHeight = maVideoCap.get(cv::CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+
+    // just in case something wrong occured
+    if (mfWidth * mfHeight == 0 )
+    {
+        mfWidth  = CAPTURED_FRAME_WIDTH;
+        mfHeight = CAPTURED_FRAME_HEIGHT;
+    }
+    maFrameSize = cv::Size(static_cast<int>(mfWidth), static_cast<int>(mfHeight));
+}
+
+double CaptureDev::getWidth(void)
+{
+    return mfWidth;
+}
+
+double CaptureDev::getHeight(void)
+{
+    return mfHeight;
+}
+
+
+cv::Size CaptureDev::getFrameSize(void)
+{
+    return maFrameSize;
+}
+
+bool CaptureDev::IsCapturePaused(void)
+{
+    return mb_paused;
+}
+
+bool CaptureDev::IsCaptureOpened(void)
+{
+    return mb_opened;
+}
+
+
+bool CaptureDev::canReadFrame(int fps, unsigned int current, unsigned int old)
+{
+    bool toReturn = false;
+
+    unsigned int delta_time = (unsigned int)(1000.0f/fps);
+    unsigned int loop_time = current - old;
+
+    std::cerr << " current_time : " << current << std::endl;
+    std::cerr << " old_time : " << old << std::endl;
+    std::cerr << " delta_time : " << delta_time << std::endl;
+    std::cerr << " loop_time : " << loop_time << std::endl;
+
+    if (delta_time <= loop_time)
+        toReturn = true;
+
+    return toReturn;
+}
