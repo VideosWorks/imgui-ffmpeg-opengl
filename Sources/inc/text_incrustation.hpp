77589fcbe7963f0ca3fde812ba534a5ba0de620e
erba <eric.bachard@free.fr>
2019-08-06 09:05:32 +0200

    Add text_incrustation.hpp

---
+++ Sources/inc/text_incrustation.hpp
@@ -0,0 +1,16 @@
+/*
+ *  File text_incrustation.hpp, belong to miniDart project
+ *  Copyright Eric Bachard, 2019 july 31st
+ *  This file is under GPL v2 license
+ * See : http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+#ifndef _TEXT_INCRUSTATION_HPP
+#define _TEXT_INCRUSTATION_HPP
+
+#include "draw_text.hpp"
+#include "string"
+
+bool text_in_video_helper(md::TextCanvas *);
+
+#endif /* _TEXT_INCRUSTATION_HPP */
