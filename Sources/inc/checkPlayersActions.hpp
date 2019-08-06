db28190a99117281032942d04108b58dc19ae8d6
erba <eric.bachard@free.fr>
2019-08-06 08:51:00 +0200

    Add checkPlayersActions.hpp

---
+++ Sources/inc/checkPlayersActions.hpp
@@ -0,0 +1,14 @@
+/*
+ * Fichier d'en tête checkPlayersActions.hpp pour le projet miniDart
+ * Auteur : Eric Bachard  / lundi 17 décembre 2018, 8:48:17 (UTC+0200)
+ * Ce document est sous Licence GPL v2
+ * voir : http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+#ifndef __CHECK_PLAYERS_ACTIONS_H__
+#define __CHECK_PLAYERS_ACTIONS_H__
+
+void checkPlayersActions(void);
+
+
+#endif  /*__CHECK_PLAYERS_ACTIONS_H__ */
